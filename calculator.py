aaaad83e2a4fefc6a20deb6a3450f786a46e7f4f
vidit_od <vidit894@gmail.com>
2021-04-20 14:37:58 +0530

    adding styles-1

--- calculator.py
+++ calculator.py
@@ -1,15 +1,36 @@
 from tkinter import *
 import time
+
 # Main Window
 window = Tk()
 window.title("Calculator")
 
+image_1 = PhotoImage(file="buttons/1.png")
+image_2 = PhotoImage(file="buttons/2.png")
+image_3 = PhotoImage(file="buttons/3.png")
+image_4 = PhotoImage(file="buttons/4.png")
+image_5 = PhotoImage(file="buttons/5.png")
+image_6 = PhotoImage(file="buttons/6.png")
+image_7 = PhotoImage(file="buttons/7.png")
+image_8 = PhotoImage(file="buttons/8.png")
+image_9 = PhotoImage(file="buttons/9.png")
+image_0 = PhotoImage(file="buttons/0.png")
+
+image_AC = PhotoImage(file="buttons/AC.png")
+image_sign = PhotoImage(file="buttons/+-.png")
+image_back = PhotoImage(file="buttons/percent.png")
+image_divide = PhotoImage(file="buttons/divide.png")
+image_multipy = PhotoImage(file="buttons/multiply.png")
+image_minus = PhotoImage(file="buttons/minus.png")
+image_add = PhotoImage(file="buttons/plus.png")
+image_equals = PhotoImage(file="buttons/equals.png")
+image_dot = PhotoImage(file="buttons/dot.png")
 # Display Tab
 display = Entry(window, width="28", borderwidth=0)
 display.grid(row=1, column=0, ipadx=30, ipady=10)
 
 # creating a frame for all the number, and oberations button
-numpad = Frame(window)
+numpad = Frame(window, bg="black")
 numpad.grid(row=2, column=0)
 history = open("history.txt", 'a')
 view_history = open("history.txt", 'r')
@@ -140,63 +161,72 @@ def clear_all():
 
 
 # buttons form 0 to 9
-button_one = Button(numpad, text="1", padx=20, pady=20,
+button_one = Button(numpad, image=image_1, borderwidth=0, bg="black",
                     command=lambda: button_click(1))
-button_two = Button(numpad, text="2", padx=20, pady=20,
+button_two = Button(numpad, image=image_2, borderwidth=0, bg="black",
                     command=lambda: button_click(2))
-button_thr = Button(numpad, text="3", padx=20, pady=20,
+button_thr = Button(numpad, image=image_3,  borderwidth=0, bg="black",
                     command=lambda: button_click(3))
-button_fou = Button(numpad, text="4", padx=20, pady=20,
+button_fou = Button(numpad, image=image_4, borderwidth=0, bg="black",
                     command=lambda: button_click(4))
-button_fiv = Button(numpad, text="5", padx=20, pady=20,
+button_fiv = Button(numpad, image=image_5, borderwidth=0, bg="black",
                     command=lambda: button_click(5))
-button_six = Button(numpad, text="6", padx=20, pady=20,
+button_six = Button(numpad, image=image_6, borderwidth=0, bg="black",
                     command=lambda: button_click(6))
-button_sev = Button(numpad, text="7", padx=20, pady=20,
+button_sev = Button(numpad, image=image_7, borderwidth=0, bg="black",
                     command=lambda: button_click(7))
-button_eig = Button(numpad, text="8", padx=20, pady=20,
+button_eig = Button(numpad, image=image_8, borderwidth=0, bg="black",
                     command=lambda: button_click(8))
-button_nin = Button(numpad, text="9", padx=20, pady=20,
+button_nin = Button(numpad, image=image_9, borderwidth=0, bg="black",
                     command=lambda: button_click(9))
-button_zer = Button(numpad, text="0", padx=20, pady=20,
+button_zer = Button(numpad, image=image_0, borderwidth=0, bg="black",
                     command=lambda: button_click(0))
 
 # Operation buttons
-plus = Button(numpad, text="+", padx=19, pady=20, command=plus)
-minus = Button(numpad, text="-", padx=20, pady=20, command=minus)
-multiply = Button(numpad, text="*", padx=20, pady=20, command=multiply)
-divide = Button(numpad, text="/", padx=20, pady=20, command=divide)
-equals = Button(numpad, text="=", padx=20, pady=52, command=equals)
-button_dot = Button(numpad, text=".", padx=22, pady=20,
+plus = Button(numpad, image=image_add, borderwidth=0, bg="black", command=plus)
+minus = Button(numpad, image=image_minus,
+               borderwidth=0, bg="black", command=minus)
+multiply = Button(numpad, image=image_multipy, borderwidth=0,
+                  bg="black", command=multiply)
+divide = Button(numpad, image=image_divide,
+                borderwidth=0, bg="black", command=divide)
+equals = Button(numpad, image=image_equals,
+                borderwidth=0, bg="black", command=equals)
+button_dot = Button(numpad, image=image_dot, borderwidth=0, bg="black",
                     command=lambda: button_click(11))
-button_sig = Button(numpad, text="+/-", padx=12, pady=20,
+button_sig = Button(numpad, image=image_sign, borderwidth=0, bg="black",
                     command=invert)
-button_bac = Button(numpad, text="<-", padx=17, pady=20,
+button_bac = Button(numpad, image=image_back, borderwidth=0, bg="black",
                     command=backspace)
-button_Alc = Button(numpad, text="AC", padx=15, pady=20,
+button_Alc = Button(numpad, image=image_AC, borderwidth=0, bg="black",
                     command=clear_all)
 
-
 # packing buttons in screen
-button_one.grid(row=4, column=0)
-button_two.grid(row=4, column=1)
-button_thr.grid(row=4, column=2)
-button_fou.grid(row=3, column=0)
-button_fiv.grid(row=3, column=1)
-button_six.grid(row=3, column=2)
-button_sev.grid(row=2, column=0)
-button_eig.grid(row=2, column=1)
-button_nin.grid(row=2, column=2)
-button_zer.grid(row=5, column=1)
-button_dot.grid(row=5, column=2)
-plus.grid(row=2, column=3)
-minus.grid(row=3, column=3)
-multiply.grid(row=4, column=3)
-divide.grid(row=5, column=3)
-button_sig.grid(row=5, column=0)
-button_bac.grid(row=3, column=4)
-button_Alc.grid(row=2, column=4)
-equals.grid(row=4, column=4, rowspan=2)
+
+
+button_Alc.grid(row=0, column=0)
+button_sig.grid(row=0, column=1)
+button_bac.grid(row=0, column=2)
+divide.grid(row=0, column=3)
+
+button_sev.grid(row=1, column=0)
+button_eig.grid(row=1, column=1)
+button_nin.grid(row=1, column=2)
+multiply.grid(row=1, column=3)
+
+button_fou.grid(row=2, column=0)
+button_fiv.grid(row=2, column=1)
+button_six.grid(row=2, column=2)
+minus.grid(row=2, column=3)
+
+button_one.grid(row=3, column=0)
+button_two.grid(row=3, column=1)
+button_thr.grid(row=3, column=2)
+plus.grid(row=3, column=3)
+
+button_zer.grid(row=4, column=0, columnspan=2)
+button_dot.grid(row=4, column=2)
+equals.grid(row=4, column=3)
 
 history.close
 window.mainloop()
